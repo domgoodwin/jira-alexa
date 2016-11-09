50dc298fb53f5d4a3723f9f18c0461f63b1abfcf
ethanduncan <ethan.duncan-stewart@capgemini.com>
2016-11-09 20:05:48 +0000

    findAllProjects db connects and init intent

---
+++ src/findAllProjects.js
@@ -0,0 +1,41 @@
+// Test running on a node js file
+console.log('File has been started');
+
+//Require
+var http = require('http');
+var localVariables = require('./localVariables');
+//var parseString = require('xml2js').parseString;
+
+// Variables
+var jsonObject = "";
+//var apiPath = '/rest/api/2/project';
+
+// Http Request Options
+var options = {
+	host: 'linux.domgoodw.in',
+	port: 8080,
+	path: '/rest/api/2/project',
+	method: 'GET',
+	json:true,
+	headers: {
+	'Authorization': 'Basic ' + localVariables.namePassword
+	}
+};
+
+var req = http.request(options, function(res) {
+	console.log('STATUS: ' + res.statusCode);
+	console.log('HEADERS: ' + JSON.stringify(res.headers));
+	res.setEncoding('utf8');
+	res.on('data', function (chunk) {
+		console.log('BODY: ' + chunk);
+		var jsonChunk = JSON.parse(JSON.stringify(chunk));
+		var data = JSON.parse(chunk);
+		for(var i = 0; i < data.length; i++) {
+            var obj = data[i];
+            console.log(obj.name);
+        }
+	});
+
+});
+
+req.end();
